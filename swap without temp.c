c0848fd9c44b13eef5f17ece601dcbbfbc459ef1
dhruvrajzala200 <142960525+dhruvrajzala200@users.noreply.github.com>
2023-08-23 09:11:37 +0530

    Add files via upload

---
+++ swap without temp.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+
+void main(){
+	int a,b;
+	printf("Enter value of a : ");
+	scanf("%d",&a);
+	printf("Enter value of b : ");
+    scanf("%d",&b);
+    printf("After swapping : value of a = %d , value of b = %d,",b,a);
+    
+	
+}
