b5f91f50edc2726975b4353138f7051586b413a0
dhruvrajzala200 <142960525+dhruvrajzala200@users.noreply.github.com>
2023-08-23 09:15:19 +0530

    Add files via upload

---
+++ area.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+void main()
+#define pie 3.14
+{
+	float r,area;
+	printf("Enter value of radius : ");
+	scanf("%f",&r);
+	area=pie*r*r;
+	printf("The area of circle is : %f",area);
+}
