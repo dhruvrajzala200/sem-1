94d575fe2676f2db7d98a8ed4345d8a4d5f35de9
dhruvrajzala200 <142960525+dhruvrajzala200@users.noreply.github.com>
2023-08-23 09:10:44 +0530

    Add files via upload

---
+++ swap 2 number.c
@@ -0,0 +1,33 @@
+#include<stdio.h>
+
+void main(){
+	int a,b,c;
+	printf("Enter value of an integer : ");
+	scanf("%d",&a);
+	printf("Enter value of an integer : ");
+	scanf("%d",&b);
+	printf("Enter value of an integer : ");
+   	scanf("%d",&c);
+   	if(a>b)
+   	{
+   		if(a>c)
+   		{
+   			printf("Largest value is : %d",a);
+		}
+		else
+		{
+   			printf("Largest value is : %d",c);
+		}
+    }    
+   else
+   {
+   	if(b>c)
+   }
+   {
+   	        printf("Largest value is : %d",b);
+   }
+   else
+   {
+   	  printf("Largest value is : %d",c);
+   }
+}
