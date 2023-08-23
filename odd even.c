a716344653ddeaef937d946259d6814c0221da76
dhruvrajzala200 <142960525+dhruvrajzala200@users.noreply.github.com>
2023-08-23 09:07:10 +0530

    Add files via upload

---
+++ odd even.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+void main(){
+	int a;
+	printf("Enter value of an integer : ");
+	scanf("%d",&a);
+	if(a%2 == 0)
+	{
+		printf("Enter number is even");
+	}
+	else
+	{
+		printf("Enter number is odd");
+	}
+	
+}
