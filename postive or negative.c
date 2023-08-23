b6b4bc9dddccadf06fc92c9b618eb66e41c6b5d8
dhruvrajzala200 <142960525+dhruvrajzala200@users.noreply.github.com>
2023-08-23 09:07:56 +0530

    Add files via upload

---
+++ postive or negative.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+void main(){
+	int a;
+	printf("Enter value of an integer : ");
+	scanf("%d",&a);
+	if(a>=0)
+	{
+		printf(" value is positive ");
+	}
+	else
+	{
+		printf(" value is negative ");
+	}
+}
