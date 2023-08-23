13108d97e68256fa4b3c78509271735cc4892d73
dhruvrajzala200 <142960525+dhruvrajzala200@users.noreply.github.com>
2023-08-23 09:09:53 +0530

    Add files via upload

---
+++ sum3.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+
+void main()
+{ 
+    int a,b,c;
+    printf("Enter first value : ");
+    scanf("%d",&a);
+    printf("Enter second value : ");
+    scanf("%d",&b);
+    c=a+b;
+    printf("The sum is : %d",c);
+}
