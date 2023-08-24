98f16cacfe3ec56b170e9aa8b4933dfce3de17c9
dhruvrajzala200 <142960525+dhruvrajzala200@users.noreply.github.com>
2023-08-24 12:05:28 +0530

    Add files via upload

---
+++ addition 2.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+
+int main(){
+	int a,b;
+	printf("Enter first value:");
+	printf("Enter second value:");
+	print("sum is %d",a+b);
+}
---
+++ addition.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+
+int main(){
+	int a,b;
+	printf("ENTER FIRST VALUE:");
+	scanf("%d",&a); 
+	printf("ENTER SECOND VALUE:");
+	scanf("%d",&b);
+	printf("sum is %d",a+b);
+}
