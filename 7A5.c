482214836b933cbc9bec0445d57d4f7d20fd6bf1
dhruvrajzala200 <142960525+dhruvrajzala200@users.noreply.github.com>
2023-09-20 08:19:39 +0530

    Add files via upload

---
+++ 7A5.c
@@ -0,0 +1,10 @@
+//Check whether number is even number or odd 
+//number using conditional operator.
+#include<stdio.h>
+
+void main(){
+	int a;
+	scanf("%d",&a);
+	(a%2==0)?printf("Even"):printf("Odd");
+	
+}
