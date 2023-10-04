5193d6ecf6bcc3172cb4a26dc4191bf23024b8a0
dhruvrajzala200 <142960525+dhruvrajzala200@users.noreply.github.com>
2023-10-04 09:02:09 +0530

    Add files via upload

---
+++ 13A.c
@@ -0,0 +1,77 @@
+//(a)
+/*****
+****
+***
+**
+*/
+#include<stdio.h>
+ void main (){
+    int j,i;
+    for(i=1;i<=5;i++){
+    	   for(j=5;j>=i;j--){
+    printf("*");
+		}
+	printf("\n");
+	}
+}
+//(b)
+//12345
+//1234
+//123
+//12
+//1
+
+#include<stdio.h>
+void main(){
+    int i,j,n=5;
+    for(i=1;i<=n;i++){
+    	for(j=1;j<=(n+1)-i;j++){
+    printf("%d",j);
+	  }
+	printf("\n");
+    }
+}
+
+//(c)
+//     *
+//    * *
+//   * * *
+//  * * * *
+// * * * * *
+
+#include<stdio.h>
+
+void main(){
+int i,j,n=5;
+    for(i=1;i<=n;i++){
+    	for(j=i;j<=(n-1);j++){
+    		printf(" ");
+		}
+	for(j=1;j<=i;j++){
+		printf(" *");
+	 }
+	    printf("\n");
+	}
+}
+//(d)
+//    1
+//   1 2
+//  1 2 3
+// 1 2 3 4
+//1 2 3 4 5
+
+#include<stdio.h>
+void main(){
+    int i,j,n=5;
+for(i=1;i<=n;i++){
+    for(j=1;j<=n-i;j++){
+      printf(" ");
+}
+   for(j=1;j<=i;j++){
+      printf("%d ",j);
+	  }
+	printf("\n");
+    }
+} 
+
+
