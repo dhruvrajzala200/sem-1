fa40ffc84c2b559fb3ba8557592ed04583871620
dhruvrajzala200 <142960525+dhruvrajzala200@users.noreply.github.com>
2023-10-10 22:42:40 +0530

    Add files via upload

---
+++ 14A1.c
@@ -0,0 +1,23 @@
+//Read n numbers from user and print in normal and reverse order.
+#include<stdio.h>
+void main(){
+	int n,i;
+	printf("How many numbers do you want to enter? ");
+	scanf("%d",&n);
+	int num[n];
+	for(i=1;i<=n;i++){
+		printf("Enter %d number =",i);
+		scanf("%d",&num[i]);
+	}
+	i=1;
+	while(i<=n){
+		printf("%d num is= %d,",i,num[i]);
+		i++;
+	}
+	printf("\n");
+	i=n;
+	while(i>0){
+		printf("%d num is= %d,",i,num[i]);
+		i--;
+	}
+}
